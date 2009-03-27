403d64ef57e70e0a8ca405261ea1c73ecf59fdfa
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-03-27 15:12:14 +0100

    trying to fix registering

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	acts_as_authentic do |conf|
-		conf.validates_length_of_password_field_options :minimum=>3
-		conf.validates_length_of_password_confirmation_field_options :minimum => 3
+		#conf.validates_length_of_password_field_options :minimum=>3
+		#conf.validates_length_of_password_confirmation_field_options :minimum => 3
 	end
 	has_many :posts
 end
