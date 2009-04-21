cf4210c9a4afe062c2f7bf2c75552465b25f2ddc
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-04-21 18:19:39 +0200

    added posts rss feed. Closes #2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,42 +2,36 @@ class PostsController < ApplicationController
   before_filter :login_required, :except => [:index, :show]
 	
   def index
-    @posts = Post.paginate :page => params[:page], :per_page =>4
+    @posts = Post.paginate :page => params[:page], :per_page =>5
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.xml  { render :xml => @posts }
+			format.rss
     end
   end
 
-  # GET /posts/1
-  # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.xml  { render :xml => @post }
     end
   end
 
-  # GET /posts/new
-  # GET /posts/new.xml
   def new
     @post = Post.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html
       format.xml  { render :xml => @post }
     end
   end
 
-  # GET /posts/1/edit
   def edit
     @post = Post.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.xml
   def create
     @post = Post.new(params[:post])
     @post.user_id = current_user.id
@@ -54,8 +48,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PUT /posts/1
-  # PUT /posts/1.xml
   def update
     @post = Post.find(params[:id])
 
@@ -71,8 +63,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.xml
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
