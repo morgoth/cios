d8da8ab75f0c83dbcdf8c204e6e03b45ad27f616
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-10-21 00:05:44 +0200

    added redirection to new domain if entered by heroku subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,44 +5,51 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   filter_parameter_logging :password, :password_confirmation
   helper_method :current_user_session, :current_user
+  before_filter :ensure_domain
   before_filter :sponsors_index, :set_locale
 
   private
+
   def sponsors_index
     @sponsors = Sponsor.all
   end
 
-    def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
 
-    def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.user
-    end
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
 
-    def login_required
-      unless current_user
-        store_location
-        flash[:notice] = t("login_required")
-        redirect_to new_user_session_url
-        return false
-      end
+  def login_required
+    unless current_user
+      store_location
+      flash[:notice] = t("login_required")
+      redirect_to new_user_session_url
+      return false
     end
+  end
 
-    def store_location
-      session[:return_to] = request.request_uri
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
+  def set_locale
+    cookies[:cios_locale] = params[:locale] if params[:locale]
+    I18n.locale = params[:locale] || cookies[:cios_locale]
+  end
+
+  def ensure_domain
+    if Rails.env == 'production' and request.env['HTTP_HOST'] != 'kscios.pl'
+      redirect_to "http://kscios.pl"
     end
-
-    def redirect_back_or_default(default)
-      redirect_to(session[:return_to] || default)
-      session[:return_to] = nil
-    end
-
-    def set_locale
-      cookies[:cios_locale] = params[:locale] if params[:locale]
-      I18n.locale = params[:locale] || cookies[:cios_locale]
-    end
-
+  end
 end
