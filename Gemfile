c53a9542216719e4a987b88512c11b9909d4d11b
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2010-05-25 17:46:02 +0200

    new bundler

--- Gemfile
+++ Gemfile
@@ -1,18 +1,23 @@
-bundle_path "vendor/bundler_gems"
+source "http://rubygems.org"
 
-gem "rails"
+gem "rails", "2.3.5"
+gem "sqlite3-ruby", :require => :sqlite3, :group => [:development, :test]
 
-gem 'haml'
+gem 'haml', "2.2.23"
 gem 'erubis'
 gem 'will_paginate'
-gem 'authlogic'
+gem 'authlogic', "2.1.3"
 gem 'picasa', ">=0.1.9"
-gem 'xml-simple', :require_as => 'xmlsimple'
-gem 'acts-as-list', :require_as => 'acts_as_list'
-gem 'RedCloth', :require_as => 'redcloth'
+gem 'xml-simple', :require => 'xmlsimple'
+gem 'acts-as-list', :require => 'acts_as_list'
+gem 'RedCloth', :require => 'redcloth'
 gem 'defensio'
 
-only :test do
+group :test do
   gem "shoulda"
   gem "factory_girl"
+end
+
+group :production do
+  gem "pg"
 end
--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,18 +1,15 @@
-require "#{File.dirname(__FILE__)}/../vendor/bundler_gems/environment"
-class Rails::Boot
-  def run
-    load_initializer
-    extend_environment
-    Rails::Initializer.run(:set_load_path)
+begin
+  require "rubygems"
+  require "bundler"
+
+  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
+    raise RuntimeError, "Your bundler version is too old." +
+     "Run `gem install bundler` to upgrade."
   end
 
-  def extend_environment
-    Rails::Initializer.class_eval do
-      old_load = instance_method(:load_environment)
-      define_method(:load_environment) do
-        Bundler.require_env RAILS_ENV
-        old_load.bind(self).call
-      end
-    end
-  end
+  # Set up load paths for all bundled gems
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+    "Did you run `bundle install`?"
 end
