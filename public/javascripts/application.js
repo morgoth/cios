c06eefd59b1d924152ae9cb60c6ed22b16be0aa0
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2011-04-02 00:19:18 +0200

    js update, removed unused devise views

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,4 @@
-//railscasts dynamic comments
-jQuery.ajaxSetup({
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-})
-
 jQuery(function($) {
-    $("input#user_session_login").focus();
-
-    $("#new_comment").submitWithAjax();
-
     // opening links in separate window
     $("a[data-popup=true]").each(function(i, element) {
         $(element).click(function (event) {
@@ -25,20 +16,12 @@ jQuery(function($) {
         });
     });
 
-  // modified from TwitterGitter by David Walsh (davidwalsh.name)
-  // courtesy of Jeremy Parrish (rrish.org)
-  function linkify(text) {
-    return text
-      .replace(/(https?:\/\/[\w\-:;?&=+.%#\/]+)/gi, '<a href="$1">$1</a>')
-      .replace(/(^|\W)@(\w+)/g, '$1<a href="http://twitter.com/$2">@$2</a>')
-      .replace(/(^|\W)#(\w+)/g, '$1#<a href="http://search.twitter.com/search?q=%23$2">$2</a>')
-  }
+    // modified from TwitterGitter by David Walsh (davidwalsh.name)
+    // courtesy of Jeremy Parrish (rrish.org)
+    function linkify(text) {
+        return text
+            .replace(/(https?:\/\/[\w\-:;?&=+.%#\/]+)/gi, '<a href="$1">$1</a>')
+            .replace(/(^|\W)@(\w+)/g, '$1<a href="http://twitter.com/$2">@$2</a>')
+            .replace(/(^|\W)#(\w+)/g, '$1#<a href="http://search.twitter.com/search?q=%23$2">$2</a>')
+    }
 });
-
-jQuery.fn.submitWithAjax = function() {
-  this.submit(function() {
-    $.post(this.action, $(this).serialize(), null, "script");
-    return false;
-  })
-  return this;
-};
