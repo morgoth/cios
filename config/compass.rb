633926ada02e6d8db31b2ff1043ee6eea670e32e
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2011-07-30 18:35:45 +0200

    moved images to assets directory

--- config/compass.rb
+++ config/compass.rb
@@ -1,10 +1,10 @@
 project_type = :rails
-# project_path = Compass::AppIntegration::Rails.root
+project_path = Compass::AppIntegration::Rails.root
 # css_dir = "tmp/stylesheets"
 # sass_dir = "app/stylesheets"
 # images_dir = "public/images"
 # javascripts_dir = "public/javascripts"
-# output_style = :compact
-# http_images_path = "/images"
-# environment = Compass::AppIntegration::Rails.env
+output_style = :compact
+http_images_path = "/assets"
+environment = Compass::AppIntegration::Rails.env
 http_path = "/"
