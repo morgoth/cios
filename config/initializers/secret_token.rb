6bc2316bf529c46eff1ab8d7a2c8ce9a4de2e13f
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2012-07-11 23:12:23 +0200

    gems update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cios::Application.config.secret_token = 'fe32ab17f3d2351ddacfcd77172155d8cf1b59d4955189c253ace36e84c4e702cc08d03a5cd0bc4ec49499ea4af5f307ee47ce0156a0b749e044b0b7bc7afd01'
+Cios::Application.config.secret_token = ENV["SECRET_TOKEN"] || "534c9a66bffb73c0a5b1c3d9679e960d"
