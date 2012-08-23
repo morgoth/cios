7eeec8a01c0bb636594d7823f909489a0f341d38
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2012-08-23 20:42:01 +0200

    updated to new picasa gem version

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -2,10 +2,16 @@ class GalleriesController < ApplicationController
   respond_to :html
 
   def index
-    respond_with @albums = Picasa.albums
+    respond_with @albums = picasa.album.list
   end
 
   def show
-    respond_with @album = Picasa.photos(:album_id => params[:id])
+    respond_with @album = picasa.album.show(params[:id])
+  end
+
+  private
+
+  def picasa
+    @picasa ||= Picasa::Client.new(user_id: "cios.gilowice")
   end
 end
--- config/initializers/picasa.rb
+++
@@ -1,3 +0,0 @@
-Picasa.config do |c|
-  c.google_user = "cios.gilowice"
-end
